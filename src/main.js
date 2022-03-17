c9167d694ff212f2574a2d02d6b3cc6744852686
byungjin <byungjin.dev@gmail.com>
2022-03-17 17:41:53 +0900

    vue-trello init

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import * as Vue from 'vue'
+import * as VueRouter from 'vue-router'
+import App from './App.vue'
+
+const Login = {
+    template: '<div>Login</div>'
+}
+const NotFound = {
+    template: '<div color=red>Page Not Found</div>'
+}
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(), //Not Hash
+    routes: [
+        {path: '/', component: App},
+        {path: '/login', component: Login},
+        {path: '/:pathMatch(.*)*', component: NotFound},
+    ]
+})
+
+const app = Vue.createApp({
+    render: ()=>Vue.h({template:'<router-view></router-view>'})
+});
+
+app.use(router);
+
+app.mount("#app");
