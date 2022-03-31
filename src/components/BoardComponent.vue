fb183595c78a6932cef7541f47d99cf0220136ec
byungjin <byungjin.dev@gmail.com>
2022-03-31 20:24:04 +0900

    router/nested

--- src/components/BoardComponent.vue
+++ src/components/BoardComponent.vue
@@ -2,6 +2,10 @@
     <div>
         Board
         <div>bid : {{bid}}</div>
+        <router-link :to="`/b/${bid}/c/1`">C1</router-link><br>
+        <router-link :to="`/b/${bid}/c/2`">C2</router-link>
+        <hr/>
+        <router-view></router-view>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@ import Login from '../components/LoginComponent.vue'
 import Home from '../components/HomeComponent.vue'
 import NotFound from '../components/NotFoundComponent.vue'
 import Board from '../components/BoardComponent.vue'
+import Card from '../components/CardComponent.vue'
 
 const router = createRouter({
     history: createWebHistory(), //Not Hash
     routes: [
         {path: '/', component: Home},
-        {path: '/b/:bid', component: Board},
+        {path: '/b/:bid', component: Board, children : [
+            {path: 'c/:cid', component: Card}
+        ]},
         {path: '/login', component: Login},
         {path: '/:pathMatch(.*)*', component: NotFound},
     ]
