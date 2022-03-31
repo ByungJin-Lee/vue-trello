0a064a95967016a7401ff086185192a4463a35b9
byungjin <byungjin.dev@gmail.com>
2022-03-31 20:04:55 +0900

    router/dynamic board

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Login from '../components/LoginComponent.vue'
 import Home from '../components/HomeComponent.vue'
 import NotFound from '../components/NotFoundComponent.vue'
+import Board from '../components/BoardComponent.vue'
 
 const router = createRouter({
     history: createWebHistory(), //Not Hash
     routes: [
         {path: '/', component: Home},
+        {path: '/b/:bid', component: Board},
         {path: '/login', component: Login},
         {path: '/:pathMatch(.*)*', component: NotFound},
     ]
