afb60db879bd30e537424142f1af1c2838ee8b20
byungjin <byungjin.dev@gmail.com>
2022-03-31 20:11:52 +0900

    board componet

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,6 +1,14 @@
 <template>
 	<div>
 		Home
+		<ul>
+			<li>
+				<router-link to='/b/1'>1</router-link>
+			</li>
+			<li>
+				<router-link to='/b/2'>2</router-link>
+			</li>
+		</ul>
 	</div>
 </template>
 
