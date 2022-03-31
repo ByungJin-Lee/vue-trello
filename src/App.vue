7c2b9b60d73f9c4a20488aabd6b30866398e1095
byungjin <byungjin.dev@gmail.com>
2022-03-31 19:45:04 +0900

    router/link

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <nav-bar></nav-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/NavbarComponent.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'nav-bar' : Navbar
+  }
 }
 </script>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <!-- Router Link
+    페이지의 업데이트를 막고,
+    해시 모드에 독립적임
+     -->
+        <router-link to='/'>Home</router-link>
+        <router-link to='/login'>Login</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
